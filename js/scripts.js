cd66821c9956cd24f0f00f91f366aa1ba9bddf3e
sharrondenice <sharrondenice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-22 22:21:14 +0000

    Prevent excerpts of protected posts from being displayed Ticket #FP-8 #8
    git-svn-id: https://plugins.svn.wordpress.org/tsp-featured-posts/tags/1.1.8@776956 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+	/* 
+	//Hide the facebook Like button for the hottest topic posts
+	jQuery("div#tspfp_article.layout2 .al2fb_like_button").css("visibility", 'hidden');
+	jQuery("div#tspfp_article.layout2 .al2fb_likers").css("visibility", 'hidden');
+	jQuery("div#tspfp_article.layout2 .al2fb_like_button").css("height", '0');
+	jQuery("div#tspfp_article.layout2 .al2fb_likers").css("height", '0');
+	*/
+
+	/* 
+	// Hide the facebook Like button for the hottest topic posts - slider
+	jQuery("div#tspfp_article.layout4 .al2fb_like_button").css("visibility", 'hidden');
+	jQuery("div#tspfp_article.layout4 .al2fb_likers").css("visibility", 'hidden');
+	jQuery("div#tspfp_article.layout4 .al2fb_like_button").css("height", '0');
+	jQuery("div#tspfp_article.layout4 .al2fb_likers").css("height", '0');
+	*/
+
+	// Hide the facebook Like button for all
+	jQuery("div#tspfp_article .al2fb_like_button").css("visibility", 'hidden');
+	jQuery("div#tspfp_article .al2fb_likers").css("visibility", 'hidden');
+	jQuery("div#tspfp_article .al2fb_like_button").css("height", '0');
+	jQuery("div#tspfp_article .al2fb_likers").css("height", '0');
+});
